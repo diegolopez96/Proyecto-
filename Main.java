ec682d586b9e9fe8063fb909aac599088d383ada
diegolopez96 <lop141222@uvg.edu.gt>
2016-08-03 01:56:53 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -25,29 +25,135 @@ public class Main {
 				//aqui se llaman a los metodos para trabajar
 				//tsid
 				
-				if (persona.analizar().substring(0,1).equals("c"))
+				
+				
+				
+				
+				/////////////////////////////////////////////////////
+				
+				
+				//######
+								//tsid
+
+				//cuando topa arriba
+				if (persona.getmov()==0 && persona.analizar().substring(0,1).equals("t"))
+				{
+					 if(persona.analizar().substring(2,3).equals("c"))
+									{
+									persona.izquierda(persona.analizar());
+									}
+					else if (persona.analizar().substring(3,4).equals("c"))
+									{
+									persona.derecha(persona.analizar());
+									}
+				}
+
+
+
+				//cuando topa izquierda
+
+				else if (persona.getmov()==1 && persona.analizar().substring(2,3).equals("t"))
+				{
+					 if(persona.analizar().substring(1,2).equals("c"))
+									{
+									persona.abajo(persona.analizar());
+									}
+					else if (persona.analizar().substring(0,1).equals("c"))
+									{
+									persona.arriba(persona.analizar());
+									}
+				}
+
+
+				//cuando topa Abajo
+
+				else if (persona.getmov()==2 && persona.analizar().substring(1,2).equals("t"))
+				{
+					 if(persona.analizar().substring(3,4).equals("c"))
+									{
+									persona.derecha(persona.analizar());
+									}
+					else if (persona.analizar().substring(2,3).equals("c"))
+									{
+									persona.izquierda(persona.analizar());
+									}
+				}
+
+
+				//cuando topa derecha
+
+				else if (persona.getmov()==3 && persona.analizar().substring(3,4).equals("t"))
+					{
+					 if(persona.analizar().substring(0,1).equals("c"))
+									{
+									persona.arriba(persona.analizar());
+									}
+					else if (persona.analizar().substring(1,2).equals("c"))
+									{
+									persona.abajo(persona.analizar());
+									}
+				}
+				
+			
+
+				//######
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				//////////////////////////////////////////////////////
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				
+				else if (persona.analizar().substring(0,1).equals("c"))
 				{
 					persona.arriba(persona.analizar());
 				}
 				
+				
 				else if(persona.analizar().substring(2,3).equals("c"))
 				{
-				persona.izquierda(persona.analizar());
+					persona.izquierda(persona.analizar());
 				}
 				
 				else if(persona.analizar().substring(1,2).equals("c"))
 				{
-				persona.abajo(persona.analizar());
+					persona.abajo(persona.analizar());
 				}
 				
 				else if (persona.analizar().substring(3,4).equals("c"))
 				{
-				persona.derecha(persona.analizar());
+					persona.derecha(persona.analizar());
 				}
+				
+				System.out.println("esto regresa analizar\n    tsid"+"\n    "+persona.analizar() +"\n\n");
+				
+				
+				
+				
+				
+				
+				
 			
 			}
 	
 	}
 	
 }
-
